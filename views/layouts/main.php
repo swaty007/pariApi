0cbc3b3b2cb1b52414dadb364190ee15cdf0da3a
swaty007 <swaty0007@gmail.com>
2019-11-22 00:39:19 +0200

    first commit

---
+++ views/layouts/main.php
@@ -0,0 +1,60 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\widgets\Alert;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php $this->registerCsrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<section id="pari-match" class="pari-match">
+    <div class="pari-match__left">
+        <?= Html::img('@web/img/pari_player.png', ['class' => 'pari-match__left--img']); ?>
+
+        <p class="pari-match--main-text">
+            We know how an entertainment looks like. Feel the satisfaction of the game.
+        </p>
+        <h2 class="pari-match__title pari-match__title--left">
+            Bet 10,000
+        </h2>
+        <h2 class="pari-match__title pari-match__title--right">
+            get 20,000
+        </h2>
+        <?= Html::img('@web/img/rectangle.png', ['class' => 'pari-match__left--background']); ?>
+    </div>
+    <div class="pari-match__right">
+        <?= Html::img('@web/img/logo_on_black.svg', ['class' => 'pari-match__logo']); ?>
+
+        <div class="pari-match__form">
+            <p class="pari-match__form--text">
+                Enter your phone number.
+                Enter the verification code that you received via SMS.
+            </p>
+            <input type="text" class="pari-match__input">
+        </div>
+    </div>
+</section>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
---
+++ web/js/scripts-bundled.js
@@ -0,0 +1,113 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./web/js/scripts.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./web/js/modules/Main.js":
+/*!********************************!*\
+  !*** ./web/js/modules/Main.js ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }\n\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }\n\nvar Main =\n/*#__PURE__*/\nfunction () {\n  function Main() {\n    _classCallCheck(this, Main);\n\n    this.events();\n  }\n\n  _createClass(Main, [{\n    key: \"events\",\n    value: function events() {\n      this.fileContainer();\n      this.spinner();\n    }\n  }, {\n    key: \"fileContainer\",\n    value: function fileContainer() {\n      $(\".fileContainer input[type=file]\").on('change', function (e) {\n        var input = this;\n\n        if (input.files && input.files[0]) {\n          var reader = new FileReader(); // if (input.files[0].size/1024/1024 > 2) {\n          //     toastr['error'](\"Файл превышает размер в 2мб\", '');\n          // }\n\n          reader.onload = function (e) {\n            $(\".fileContainer .fileContainer__img\").attr('src', e.target.result).show();\n            $(\".fileContainer .fileContainer__text--select\").hide();\n            $(\".fileContainer .fileContainer__text--name\").addClass('loaded');\n          };\n\n          reader.readAsDataURL(input.files[0]);\n        }\n      });\n    }\n  }, {\n    key: \"spinner\",\n    value: function spinner() {\n      $(\".spinner input\").spinner({\n        min: 1,\n        max: 50,\n        start: 1,\n        change: function change(event, ui) {\n          $('label[for=' + this.id + ']').text(\"$\" + this.value * this.dataset.price);\n        },\n        spin: function spin(event, ui) {\n          $('label[for=' + this.id + ']').text(\"$\" + this.value * this.dataset.price);\n        }\n      });\n    }\n  }]);\n\n  return Main;\n}();\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Main);\n\n//# sourceURL=webpack:///./web/js/modules/Main.js?");
+
+/***/ }),
+
+/***/ "./web/js/scripts.js":
+/*!***************************!*\
+  !*** ./web/js/scripts.js ***!
+  \***************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_Main__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/Main */ \"./web/js/modules/Main.js\");\n// 3rd party packages from NPM\n// import $ from \"jquery\";\n// import WOW from \"wowjs\";\n// import tocca from \"tocca\";\n// Our modules / classes\n// import Anim from \"./modules/Anim\";\n// import HowWeWorkCar from \"./modules/HowWeWorkCar\";\n// import Popups from \"./modules/Popups\";\n// import Validate from \"./modules/Validate\";\n // import Cursor from \"./modules/Cursor\";\n// Instantiate a new object using our modules/classes\n\nvar main = new _modules_Main__WEBPACK_IMPORTED_MODULE_0__[\"default\"](); // const hwwcar = new HowWeWorkCar();\n// const filter = new Filter();\n// const popups = new Popups();\n// const validate = new Validate();\n// const calculator = new Calculator();\n// const forms = new Forms();\n// const cursor = new Cursor();\n\nfunction showToastr(msg) {\n  if (msg.msg === 'ok') {\n    toastr['success'](msg.status, '');\n  } else if (msg.msg === 'error') {\n    toastr['error'](msg.status, '');\n  }\n}\n\nfunction finishPjax(el) {\n  if (typeof $.pjax !== 'undefined') {\n    if (el !== undefined) {\n      $.pjax.reload({\n        container: el\n      });\n    } else {\n      $.pjax.reload({\n        container: '#p0'\n      });\n    }\n  }\n}\n\n$(document).on('click', \"#create_market\", function (e) {\n  e.preventDefault();\n  var data = {\n    title: $('#market_name').val(),\n    type: $('#market_type').val(),\n    description: $('#market_description').val(),\n    cost: $('#market_cost').val(),\n    time_action: $('#market_time_action').val(),\n    count_api: $('#market_count_api').val(),\n    file: $(\"#market_file\")[0].files[0]\n  };\n  var formData = new FormData();\n  formData.append(\"title\", data.title);\n  formData.append(\"type\", data.type);\n  formData.append(\"description\", data.description);\n  formData.append(\"cost\", data.cost);\n  formData.append(\"time_action\", data.time_action);\n  formData.append(\"count_api\", data.count_api);\n  formData.append(\"file\", data.file);\n  console.log(data);\n  $.ajax({\n    type: \"POST\",\n    url: \"/market/create-market\",\n    cache: false,\n    processData: false,\n    dataType: 'json',\n    contentType: false,\n    data: formData,\n    success: function success(msg) {\n      console.log(msg);\n      showToastr(msg);\n      finishPjax();\n      closeModal($('#market-create'));\n    }\n  });\n});\n\n//# sourceURL=webpack:///./web/js/scripts.js?");
+
+/***/ })
+
+/******/ });
